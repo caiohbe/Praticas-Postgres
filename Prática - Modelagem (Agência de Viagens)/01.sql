dc2c975810f16fa0d284918c595a3da5c9a47833
caiohbe <caiohbe@gmail.com>
2022-12-21 20:56:24 -0300

    Prática Modelagem (Agência de Viagens)

---
+++ Prática - Modelagem (Agência de Viagens)/01.sql
@@ -0,0 +1,21 @@
+CREATE TABLE airports (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    abbreviation VARCHAR(10) UNIQUE NOT NULL
+)
+
+CREATE TABLE flightCompanies (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    abbreviation VARCHAR(10) UNIQUE NOT NULL
+)
+
+CREATE TABLE flightCompanies (
+    id SERIAL PRIMARY KEY,
+    key TEXT NOT NULL,
+    takeOffDate TIMESTAMP NOT NULL,
+    landingDate TIMESTAMP NOT NULL,
+    airportFromId INTEGER NOT NULL REFERENCES airports(id),
+    airportToId INTEGER NOT NULL REFERENCES airports(id),
+    companyId INTEGER NOT NULL REFERENCES flightCompanies(id)
+)
